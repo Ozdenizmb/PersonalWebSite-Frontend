5570b6a7f278de3e1041aac82e24bad781371a4c
Mehmet Baran Özdeniz <ozdenizm6@gmail.com>
2024-08-16 16:53:49 +0300

    Request gönderilecek server adı ve port bilgisi .env dosyasında tutularak gizlendi

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = '<YOUR_DOMAIN_NAME_AND_BACKEND_PORT_ADDRESS>';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const signUpUser = (body) => {
     return axios.post(`${API_BASE_URL}/api/v1/users/signup/user`, body, {
