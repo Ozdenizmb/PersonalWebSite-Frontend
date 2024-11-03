5c57344d360d25643e1830836f7695b1e56704d1
Mehmet Baran Özdeniz <ozdenizm6@gmail.com>
2024-11-03 21:24:01 +0300

    Topbar degistirildi

--- src/components/ContactMessage.js
+++ src/components/ContactMessage.js
@@ -4,7 +4,7 @@ import ContactMessageFeed from "./ContactMessageFeed";
 const ContactMessage = () => {
     return(
         <div className="container mt-4">
-            <h1 className="text-center mb-5">Projeler</h1>
+            <h1 className="text-center mb-5">Mesajlar</h1>
             <ContactMessageFeed />
         </div>
     );
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { logoutSuccess } from '../redux/authActions';
-import skyForTopbar from '../images/sky.webp';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faInfoCircle, faProjectDiagram, faPhone, faSignInAlt, faUserPlus, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faInfoCircle, faProjectDiagram, faPhone, faSignInAlt, faUserPlus, faSignOutAlt, faBars, faChevronDown, faUserCircle  } from '@fortawesome/free-solid-svg-icons';
 import '../style/Components.css';
 import ProfileImage from './ProfileImage';
 
@@ -16,15 +15,30 @@ const TopBar = () => {
     }));
 
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const [profileMenuOpen, setProfileMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
+    };
+
+    const toggleProfileMenu = () => {
+        setProfileMenuOpen(!profileMenuOpen);
     };
 
     const dispatch = useDispatch();
     const onLogoutSuccess = () => {
         dispatch(logoutSuccess());
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <div id="topbar">
@@ -33,14 +47,50 @@ const TopBar = () => {
                     <Link to="/" className="brand-name">
                         <h3 className="logo">baran<span>ozdeniz</span></h3>
                     </Link>
-                    <div className="hamburger-menu" onClick={toggleMenu}>
-                        <div className="bar"></div>
-                    </div>
+                    {isMobile ? (
+                        <div className="toggle-menu" onClick={toggleMenu}>
+                            <FontAwesomeIcon icon={faBars} className={`toggle-menu-icon fa-2x ${menuOpen ? "text-light" : "text-dark"}`} />
+                        </div>
+                    ): (
+                        <div className="topbar-links">
+                            <div className="topbar-links-item">
+                                <Link to="/"><FontAwesomeIcon icon={faHome} className="me-2" />Anasayfa</Link>
+                                <Link to="/about"><FontAwesomeIcon icon={faInfoCircle} className="me-2" />Hakkımda</Link>
+                                <Link to="/projects"><FontAwesomeIcon icon={faProjectDiagram} className="me-2" />Projeler</Link>
+                                <Link to="/contact"><FontAwesomeIcon icon={faPhone} className="me-2" />İletişim</Link>
+                                {!isLoggedIn && (
+                                    <>
+                                        <Link to="/signup"><FontAwesomeIcon icon={faUserPlus} className="me-2" />Kaydol</Link>
+                                        <Link to="/login"><FontAwesomeIcon icon={faSignInAlt} className="me-2" />Giriş Yap</Link>
+                                    </>
+                                )}
+                            </div>
+                            
+                            {isLoggedIn && (
+                                <div className="d-flex align-items-center">
+                                    <div className="profile-dropdown" onClick={toggleProfileMenu}>
+                                        <ProfileImage email={email} width={"32"} height={"32"} tempImage={imageUrl} imageCss="m-auto me-2" />
+                                        <FontAwesomeIcon icon={faChevronDown} />
+                                    </div>
+                                    {profileMenuOpen && (
+                                        <div className="profile-menu">
+                                            <Link to={`/profile/${email}`} onClick={() => setProfileMenuOpen(false)}>
+                                                <FontAwesomeIcon icon={faUserCircle} className="me-2" />
+                                                Profil
+                                            </Link>
+                                            
+                                            <Link to="/" onClick={onLogoutSuccess}>
+                                                <FontAwesomeIcon icon={faSignOutAlt} className="me-2" />
+                                                Çıkış Yap
+                                            </Link>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                    )}
                 </div>
             </nav>
-            <header>
-                <img src={skyForTopbar} className="sky translate" data-speed="0.5" alt="" />
-            </header>
             <div className={`side-menu ${menuOpen ? 'open' : ''}`}>
                 <ul className="text-center">
                     <li>
